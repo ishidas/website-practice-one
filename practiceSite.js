5fa2c8530255958c30f5433298a6904f704fdb97
Sawako <ssagli@uw.edu>
2019-04-11 21:17:51 -0700

    adding create button instruction

---
+++ practiceSite.js
@@ -0,0 +1,11 @@
+//******
+//1. create <div> element inside of <body>
+//2. create <button> element inside of that div you created in step 1.
+//3. Give the button element "id" called "changeColor"
+//4. inside of this JS file, get the button element by id and add a click eventListener
+//5. when a user clicks on the button, alert should pop up in the browser, and background color should change
+//   to color of your choice.
+//Bonus - you can also add a condition using "if else" statement.
+//  condition is:  if the color of your choice is already used as background color,
+//  change the background color back to white.
+//*****
